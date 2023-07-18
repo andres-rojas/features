8e6952aaf3d99c5465ce7b4cf3451b9347ee3746
Andres Rojas <andres@conpat.io>
2023-07-18 14:40:37 -0700

    Extend prettier tests

--- test/prettier/v3-plugin.sh
+++ test/prettier/v3-plugin.sh
@@ -4,6 +4,8 @@ set -e
 
 source dev-container-features-test-lib
 
+check "prettier --version = 3.0.0" [ "$(prettier --version)" = "3.0.0" ]
+
 check "npm list --parseable --depth 0  | grep prettier-plugin-ini" npm list --parseable --depth 0 | grep "prettier-plugin-ini"
 
 reportResults
